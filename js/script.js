5b350331b23f5c010d4bc33f8d3cbb46d1ba1691
Arron Johnson <arronjohnson.uk@gmail.com>
2022-05-28 09:17:12 +0100

    Add computerPlay function
    Generates a random integer between 0-2 (inclusive), and uses this
    value to return a choice from an array.
    
    Possible choices are 'rock', 'paper', or 'scissors'.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+const choices = ["rock", "paper", "scissors"];
+
+function computerPlay() {
+    let randomInt = Math.floor(Math.random() * 3);
+    
+    return choices[randomInt];
+}

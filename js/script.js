f9cff3bee800eb295932930bb50deb7bf5ca62a1
Arron Johnson <arronjohnson.uk@gmail.com>
2022-05-31 17:20:45 +0100

    Update game script to support the new UI
    - Added event handlers for the different buttons.
    - Scores and output messages are updated after each round winner is
    determined.
    - Once someone reaches 5 points the overall winner is alerted and the
    entire game resets.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-const choices = ["rock", "paper", "scissors"];
+// keep track of the scores until an overall winner is decided
+let playerScore = 0;
+let computerScore = 0;
 
 // return a randomly generated choice for the computer
 function computerPlay() {
-  let randomInt = Math.floor(Math.random() * 3);
-
+  const choices = ["rock", "paper", "scissors"];
+  const randomInt = Math.floor(Math.random() * 3);
   return choices[randomInt];
 }
 
@@ -22,54 +24,73 @@ function playRound(playerSelection, computerSelection) {
   }
 
   // check win conditions for the player
-  let rockWin = playerSelection === "rock" && computerSelection == "scissors";
-  let paperWin = playerSelection === "paper" && computerSelection == "rock";
-  let scissorsWin =
+  const rockWin = playerSelection === "rock" && computerSelection == "scissors";
+  const paperWin = playerSelection === "paper" && computerSelection == "rock";
+  const scissorsWin =
     playerSelection === "scissors" && computerSelection == "paper";
 
   if (rockWin || paperWin || scissorsWin) {
+    playerScore++;
     return "player";
   }
 
   // if the player didn't win then the computer must have
+  computerScore++;
   return "computer";
 }
 
-/*
-  play a game consisting of 5 sequential rounds
-  
-  scores are updated after each round until the overall
-  winner is found and results printed
-*/
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
+// ui implementation
+const outputPara = document.querySelector(".output p");
+const startingMessage = outputPara.innerHTML;
+const buttons = document.querySelectorAll("button");
 
-  // play 5 rounds of the game
-  for (let i = 0; i < 5; i++) {
-    let playerSelection =
-      prompt("Enter your choice of rock, paper, or scissors:", "") || "rock";
-    let computerSelection = computerPlay();
-    let roundWinner = playRound(playerSelection, computerSelection);
+buttons.forEach((button) =>
+  button.addEventListener("click", (e) => onClick(e))
+);
 
-    if (roundWinner === "draw") {
-      console.log(`Draw! You both chose ${playerSelection}`);
-    } else if (roundWinner === "player") {
-      console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-      playerScore++;
-    } else {
-      console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-      computerScore++;
-    }
+function onClick(e) {
+  const playerSelection = e.target.className;
+  const computerSelection = computerPlay();
+  const roundWinner = playRound(playerSelection, computerSelection);
+
+  updateScores();
+  updateOutput(playerSelection, computerSelection, roundWinner);
+
+  if (playerScore == 5 || computerScore == 5) {
+    setTimeout(resetGame, 100);
   }
+}
 
-  // determine the overall winner using the scores and print the results
-  let overallWinner;
-  if (playerScore === computerScore) {
-    overallWinner = "Draw";
-  } else {
-    overallWinner = playerScore > computerScore ? "Player" : "Computer";
+function updateOutput(playerSelection, computerSelection, roundWinner) {
+  let text;
+  switch (roundWinner) {
+    case "player":
+      text = `You win! ${playerSelection} beats ${computerSelection}`;
+      break;
+    case "computer":
+      text = `You lose! ${computerSelection} beats ${playerSelection}`;
+      break;
+    default:
+      text = `Draw! You both chose ${playerSelection}`;
   }
-  console.log(`[Scores] Player: ${playerScore} | Computer: ${computerScore}`);
-  console.log(`The overall winner is: ${overallWinner}`);
+  outputPara.textContent = text;
+}
+
+function updateScores() {
+  const playerSpan = document.querySelector(".playerScore");
+  const computerSpan = document.querySelector(".computerScore");
+  playerSpan.textContent = playerScore;
+  computerSpan.textContent = computerScore;
+}
+
+function resetGame() {
+  const overallWinner =
+    playerScore > computerScore
+      ? "You are the overall winner!"
+      : "You lost... the computer is the overall winner :(";
+  alert(overallWinner);
+  playerScore = 0;
+  computerScore = 0;
+  updateScores();
+  outputPara.innerHTML = startingMessage;
 }
